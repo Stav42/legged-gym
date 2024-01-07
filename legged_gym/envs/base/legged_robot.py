4f523cf90cbd5905ac89a86a4fb8f24b53550531
Stav42 <vaditya@iitk.ac.in>
2024-01-07 15:17:15 +0530

    DOF Properties setting

--- legged_gym/envs/base/legged_robot.py
+++ legged_gym/envs/base/legged_robot.py
@@ -902,6 +902,8 @@ class LeggedRobot(BaseTask):
             rigid_shape_props = self._process_rigid_shape_props(rigid_shape_props_asset, i)
             self.gym.set_asset_rigid_shape_properties(robot_asset, rigid_shape_props)
             actor_handle = self.gym.create_actor(env_handle, robot_asset, start_pose, self.cfg.asset.name, i, self.cfg.asset.self_collisions, 0)
+
+            ## Setting DOF Properties
             dof_props = self._process_dof_props(dof_props_asset, i)
             self.gym.set_actor_dof_properties(env_handle, actor_handle, dof_props)
             body_props = self.gym.get_actor_rigid_body_properties(env_handle, actor_handle)
